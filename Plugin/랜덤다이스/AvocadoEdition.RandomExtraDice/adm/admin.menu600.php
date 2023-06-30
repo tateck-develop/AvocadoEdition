325eebfdfadcc6c8550cc6af8624b5d0b9fd1a1f
박현정 <tateck09@gmail.com>
2023-06-30 12:36:39 +0900

    플러그인 폴더 삭제
    설치에 혼동을 줄 수 있어, 플러그인 폴더를 삭제 후 새로운 레지스트리로 분리

--- Plugin/랜덤다이스/AvocadoEdition.RandomExtraDice/adm/admin.menu600.php
+++
@@ -1,8 +0,0 @@
-<?php
-$menu['menu600'] = array (
-	array('600000', '플러그인관리', ''.G5_ADMIN_URL.'', ''),
-	array('600200', '랜덤주사위관리', ''.G5_ADMIN_URL.'/random_list.php', '')
-);
-
-
-?>
--- Plugin/랜덤다이스/AvocadoEdition.RandomExtraDice/skin/board/mmb_random/delete_comment.tail.skin.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
-goto_url('./board.php?bo_table='.$bo_table.'&amp;'.$qstr.'&amp;#log_'.$write['wr_parent']);
-?>
